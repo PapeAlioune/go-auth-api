e14534e9ec65c95d59f15b51f6b820b643600c8c
PapeAlioune <badaradiakhate14@gmail.com>
2021-11-29 15:42:24 +0000

    Update..

--- database/connection.go
+++ database/connection.go
@@ -11,7 +11,7 @@ var (
 )
 
 func Connect() {
-	connection, err := gorm.Open(mysql.Open("root:goapisecret@tcp(127.0.0.1:52000)/go_rest?multiStatements=true"), &gorm.Config{})
+	connection, err := gorm.Open(mysql.Open("root:goapisecret@tcp(localhost:3306)/go_rest?multiStatements=true"), &gorm.Config{})
 
 	if err != nil {
 		panic("could not connect to the database")
